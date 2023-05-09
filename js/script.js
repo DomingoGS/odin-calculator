7a9fa0320b4f4c4ddd5270cac543eaf57b853999
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-05-09 17:57:11 +0200

    Update TODO list inside scripts.js

--- js/script.js
+++ js/script.js
@@ -182,5 +182,6 @@ function fitInDisplay(value) {
 
 // TO DO:
 //   1. Fix still being able to type decimal when display is at max (8 digits)
+//   2. Make decimal point disappear if operator is pressed when no other digits have been typed after the decimal point.
 
 // Testing code
